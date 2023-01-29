5733ab94dac4782d3f4b793d61ff0f4b2e657d4a
Java Babykish <bkrevsun@gmail.com>
2023-01-29 17:49:49 +0400

    db fix#2 trial

--- src/db.ts
+++ src/db.ts
@@ -6,12 +6,14 @@ import { emmiter } from './controller/dashboardController'
 
 let searchQuery: any
 const sequelize = new Sequelize(
-	process.env.DB_NAME!,
-	process.env.DB_USERNAME!,
-	process.env.DB_PASSWORD,
+	process.env.DB_NAME! || 'northwind_traders_db',
+	process.env.DB_USERNAME! || 'postgres',
+	process.env.DB_PASSWORD || 'guestdxb',
 	{
-		host: process.env.DB_HOST,
-		port: +process.env.DB_PORT!,
+		host:
+			process.env.DB_HOST ||
+			'northwind-traders-db.cozbnpykax7t.eu-west-1.rds.amazonaws.com',
+		port: +process.env.DB_PORT! || 5432,
 		dialect: 'postgres',
 		dialectModule: pg,
 		define: {
